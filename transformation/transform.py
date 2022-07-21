78569e997f35fa31a0c7de75012722f9e006d18a
warren yun <wyun2006@gmail.com>
2022-07-21 01:57:57 -0400

    transformation done

--- transformation/transform.py
+++ transformation/transform.py
@@ -1,18 +1,10 @@
-flag = "hello"
-test = ''.join([chr((ord(flag[i]) << 8) + ord(flag[i + 1])) for i in range(0, len(flag)-1, 2)])
-print(test)
+# decode_msg = ''.join([chr((ord(flag[i]) << 8) + ord(flag[i + 1])) for i in range(0, len(flag)-1, 2)])
 
+encoded = "灩捯䍔䙻ㄶ形楴獟楮獴㌴摟潦弸弲㘶㠴挲ぽ"
+flag = ""
+for i in range(0, len(encoded)):
+  char1 = ord(encoded[i]) >> 8
+  flag += chr(char1)
+  flag += chr(ord(encoded[i]) - (char1 << 8))
 
-# flag = "灩捯䍔䙻ㄶ形楴獟楮獴㌴摟潦弸弲㘶㠴挲ぽ"
-for i in range(0, len(test)):
-  print(chr((ord(test[i]) >> 8)))
-  print(chr(ord(test[i])-(ord(test[i]) >> 8) >> 8))
-  # print(((ord(test[i]))))
-  
-  # print(chr((ord(test[i]) - (ord(test[i]) >> 8)) >> 8))
-  
-  # print(chr(ord(flag[i+1]) >> 8))
-  # print(flag[i] + ": " + str(i))
-  # print(flag[i+1] + ": " + str(i+1))
-  # print(chr(ord(flag[i])>>8))
-  # print( (chr( ord(flag[i])>>8-(ord(flag[i+1])>>8)  )))
+print(flag)
